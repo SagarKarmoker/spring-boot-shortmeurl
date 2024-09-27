230e91cf600f33552e1fbc83e8ed1868acce4c83
Sagar Karmoker <sagarkarmoker2001@gmail.com>
2024-09-28 03:47:38 +0600

    Url shorter project using spring boot, redis and postgresSQL

---
+++ src/test/java/com/sagar/shorttheurl/ShortTheUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sagar.shorttheurl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortTheUrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
