d534def93dea04547e9786122d6db4a2011074d9
Sagar Karmoker <sagarkarmoker2001@gmail.com>
2024-09-28 19:07:11 +0600

    dotenv: added

--- src/main/java/com/sagar/shorttheurl/ShortTheUrlApplication.java
+++ src/main/java/com/sagar/shorttheurl/ShortTheUrlApplication.java
@@ -1,5 +1,6 @@
 package com.sagar.shorttheurl;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ShortTheUrlApplication {
 
 	public static void main(String[] args) {
+		// Load .env file
+		Dotenv dotenv = Dotenv.load();
+
+		// Set environment variables
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		SpringApplication.run(ShortTheUrlApplication.class, args);
 	}
 
