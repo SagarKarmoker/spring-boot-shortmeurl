7dba0eb428cfb550e97428c9b7eef513306e15a4
Sagar Karmoker <sagarkarmoker2001@gmail.com>
2024-09-28 22:11:18 +0600

    updated cros url

--- src/main/java/com/sagar/shorttheurl/controller/ShortUrlController.java
+++ src/main/java/com/sagar/shorttheurl/controller/ShortUrlController.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1")
-@CrossOrigin(origins = "https://shortmeurl.up.railway.app")
+//@CrossOrigin(origins = "https://shortmeurl.up.railway.app")
+//@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class ShortUrlController {
 
     @Autowired
