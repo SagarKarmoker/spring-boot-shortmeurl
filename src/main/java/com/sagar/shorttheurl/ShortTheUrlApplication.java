55b433bc72a077396b9b7237498dab3c95f52db6
Sagar Karmoker <sagarkarmoker2001@gmail.com>
2024-09-28 21:38:48 +0600

    update env

--- src/main/java/com/sagar/shorttheurl/ShortTheUrlApplication.java
+++ src/main/java/com/sagar/shorttheurl/ShortTheUrlApplication.java
@@ -9,12 +9,12 @@ public class ShortTheUrlApplication {
 
 	public static void main(String[] args) {
 		// Load .env file
-		Dotenv dotenv = Dotenv.load();
-
-		// Set environment variables
-		System.setProperty("DB_URL", dotenv.get("DB_URL"));
-		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+//		Dotenv dotenv = Dotenv.load();
+//
+//		// Set environment variables
+//		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+//		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+//		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		SpringApplication.run(ShortTheUrlApplication.class, args);
 	}
 
